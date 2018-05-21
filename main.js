240bb8fcbab2eece6efde208d24c64017ecf308e
keyuanyuan <229497835@qq.com>
2018-05-21 10:45:47 +0800

    优化下js代码

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
 let $images = $('#slides>img')
-//console.log($images)常用console打印检查是否取到
 let $buttons = $('#buttons>button')
 let $slides = $('#slides')
-
-let $firstCopy = $images.eq(0).clone(true)
-let $lastCopy = $images.eq(2).clone(true)
-$slides.append($firstCopy)
-$slides.prepend($lastCopy)
-
 let current = 0
+
+makeFakeSlides()
 $slides.css({transform:'translateX(-400px)'})
-$buttons.eq(0).on('click',function() {
-    if(current == 2) {
-        console.log('从最后一张到第一张')
-        $slides.css({transform:'translateX(-1600px)'})
-            .one('transitionend',function() {
-                $slides.hide()
-                    .offset()
-                $slides.css({transform:'translateX(-400px)'})
-                    .show()
-            //hide和show之间加offset防止两个动作被合并，产生bug
-            })
-    } else {
-        $slides.css({transform:'translateX(-400px)'})
-    }
-    current = 0
-})
-$buttons.eq(1).on('click',function() {
-    $slides.css({transform:'translateX(-800px)'})
-    current = 1
-})
-$buttons.eq(2).on('click',function() {
-    if(current == 0) {
-        console.log('从第一张到最后一张')
-        $slides.css({transform:'translateX(0px)'})
-            .one('transitionend',function() {
-                $slides.hide()
-                    .offset()
-                $slides.css({transform:'translateX(-1200px)'})
-                    .show()
-            })
-    } else {
-        $slides.css({transform:'translateX(-1200px)'})
-    }
-    current = 2
-})
+bindEvents()
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
+function bindEvents(){
+    $('#buttons').on('click','button',function(e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        if(current === $buttons.length -1 && index === 0) {
+        //从最后一张到第一张,经过克隆的第一张过渡到真的第一张
+            $slides.css({transform:`translateX(${-($buttons.length+1)*400}px)`})
+                .one('transitionend',function() {
+                    //将过渡过程隐藏，使用offset防止hide和show动作被合并
+                    $slides.hide()
+                        .offset()
+                    $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+                        .show()   
+                })
+        } else if(current === 0 && index === $buttons.length - 1) {
+            //从第一张到最后一张,经过克隆的最后一张过渡到真的最后一张
+            $slides.css({transform:'translateX(0px)'})
+                .one('transitionend',function() {
+                    $slides.hide()
+                        .offset()
+                    $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+                        .show()
+                })
+        } else {
+            $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+        }
+        current = index
+    })
+}
